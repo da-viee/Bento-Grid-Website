9a5e5cfa8b0befd5dcf163139a3a6777aea67cf3
da-viee <davidadeyemi.a@gmail.com>
2026-06-15 23:37:31 +0100

    chore: add vercel configuration

--- server.js
+++ server.js
@@ -376,6 +376,10 @@ app.get('/blog/:slug', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
